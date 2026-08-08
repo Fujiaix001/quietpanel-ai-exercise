9c33e9071e07ca67e521eaeb4cdfdb5d327dfb10
Fujiaix001 <307279363+Fujiaix001@users.noreply.github.com>
2026-08-08 19:12:33 +0800

    Revert "feat: read private album photos in v9.0.13"
    This reverts commit caa573c07d390fb73681b9c09132cc7c6be75ff5.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.quietpanel.client"
         minSdk = 17
         targetSdk = 36
-        versionCode = 9013
-        versionName = "9.0.13"
+        versionCode = 9012
+        versionName = "9.0.12"
     }
 
     flavorDimensions += "distribution"
